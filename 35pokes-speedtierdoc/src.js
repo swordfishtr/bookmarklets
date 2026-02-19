0837f0475100eee75cd55795177674bad507f8fa
demir <demir.ab@protonmail.com>
2026-02-19 22:57:09 +0300

    Use Showdown's UI to workaround focus errors

--- 35pokes-speedtierdoc/src.js
+++ 35pokes-speedtierdoc/src.js
@@ -1,4 +1,4 @@
-javascript:void async function(){try{
+javascript:void function(){try{
 	// Header row
 	const hcell = v => `<td style="border-left:solid #d6d6d6 0.6000000000000001pt;border-right:solid #d6d6d6 0.6000000000000001pt;border-bottom:solid #6363b0 1.2000000000000002pt;border-top:solid #d6d6d6 0.6000000000000001pt;vertical-align:top;background-color:#ebebf7;padding:2pt 2pt 2pt 2pt;overflow:hidden;overflow-wrap:break-word;"><p dir="ltr" style="line-height:1.38;text-align: center;margin-top:0pt;margin-bottom:0pt;"><span style="font-size:11.5pt;font-family:Roboto,sans-serif;color:#141414;background-color:transparent;font-weight:700;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">${v}</span></p></td>`;
 	const hrow = vs => `<tr style="height:20.25pt">${vs.slice(0,8).map(v=>hcell(v)).join('')}</tr>`;
@@ -12,19 +12,13 @@ javascript:void async function(){try{
 
 	const sprite = p => `<span style="border:none;display:inline-block;overflow:hidden;width:40px;height:30px;"><img alt=":Drampa:" src="https://www.smogon.com/forums/media/minisprites/${p.spriteid}.png" width="40" height="30" style="margin-left:0px;margin-top:0px;" /></span>`;
 
-	const sleep = (ms) => new Promise((res)=>setTimeout(res, ms));
-
 	if (!app?.rooms?.teambuilder?.curTeam)
 		throw new Error('Please navigate to a team in Pokemon Showdown. Make sure the team is for the correct format!');
 	const tb = app.rooms.teambuilder;
 	const t = tb.curTeam;
 	const s = new DexSearch('pokemon',t.format).typedSearch;
-	const input = prompt('Input a comma-separated list of Pokemon.');
-	if (!input) throw new Error('Aborted');
-	const roster = input.split(',').map(p=>t.dex.species.get(p));
-	if (roster.some(p=>!p.exists)) throw new Error(`Unknown Pokemon: ${roster.filter(p=>!p.exists).join(', ')}`);
 	const vs = [];
-	const addv = async (p,n,iv,ev,mod) => vs.push([
+	const addv = (p,n,iv,ev,mod) => vs.push([
 		Math.trunc((1 + 0.5 * mod) * tb.getStat('spe',{species:p.id,ivs:{spe:iv}},ev,n===true?1.1:n===false?0.9:1)),
 		sprite(p),
 		p.name,
@@ -35,35 +29,49 @@ javascript:void async function(){try{
 		mod>0?`+${mod}`:mod,
 		p.num, // used for sorting
 	]);
-	for (const p of roster) {
-		if (
-			['agility','autotomize','rockpolish','dragondance','rapidspin','scaleshot'].some(m=>s.canLearn(p.id,m)) ||
-			Object.values(p.abilities).map(toID).some(a=>['unburden','swiftswim','chlorophyll','sandrush','slushrush'].includes(a))
-		) {
-			await addv(p,true,31,252,2);
-			await addv(p,null,31,252,2);
+	let i = 600;
+	const gen = async (input) => {
+		if (!input) return app.addPopupMessage('Aborted');
+		const roster = input.split(',').map(p=>t.dex.species.get(p));
+		if (roster.some(p=>!p.exists)) return app.addPopupMessage(`Unknown Pokemon: ${roster.filter(p=>!p.exists).join(', ')}`);
+		for (const p of roster) {
+			if (
+				['agility','autotomize','rockpolish','dragondance','rapidspin','scaleshot'].some(m=>s.canLearn(p.id,m)) ||
+				Object.values(p.abilities).map(toID).some(a=>['unburden','swiftswim','chlorophyll','sandrush','slushrush'].includes(a))
+			) {
+				addv(p,true,31,252,2);
+				addv(p,null,31,252,2);
+			}
+			addv(p,true,31,252,1);
+			addv(p,null,31,252,1);
+			addv(p,true,31,252,0);
+			addv(p,null,31,252,0);
+			addv(p,null,31,0,0);
+			if (p.baseStats.spe<=60 && Math.min(...Object.values(p.baseStats))===p.baseStats.spe) {
+				addv(p,false,0,0,0);
+			}
 		}
-		await addv(p,true,31,252,1);
-		await addv(p,null,31,252,1);
-		await addv(p,true,31,252,0);
-		await addv(p,null,31,252,0);
-		await addv(p,null,31,0,0);
-		if (p.baseStats.spe<=60 && Math.min(...Object.values(p.baseStats))===p.baseStats.spe) {
-			await addv(p,false,0,0,0);
+		vs.sort((a,b)=>a[0]-b[0]||a[8]-b[8]).reverse();
+		next();
+	};
+	const next = () => {
+		let c = [];
+		while (!c.length && i >= 0) {
+			c = vs.filter(v=>v[0]>=i&&(i===600||v[0]<i+100));
+			i -= 100;
 		}
-	}
-	vs.sort((a,b)=>a[0]-b[0]||a[8]-b[8]).reverse();
-	for (let i = 600; i >= 0; i -= 100) {
-		let msg = `between ${i} and ${i+100}`;
-		if (i===600) msg = 'above 600';
-		if (i===0) msg = 'below 100';
-		msg = `Generated table for speed tiers ${msg} and copied to your clipboard! Paste directly into a google doc.`;
-		if (i>0) msg += `\nPress OK for the next table.`;
-		const c = vs.filter(v=>v[0]>=i&&(i===600||v[0]<i+100));
-		if (!c.length) continue;
-		await sleep(2000);
+		if (c.length) copy(c);
+		else app.addPopupMessage('All done! Have a good day now :3');
+	};
+	const copy = async (c) => {
 		await navigator.clipboard.write([new ClipboardItem({'text/html':table(c)})]);
-		alert(msg);
-	}
-	alert('All done! Have a good day now :3');
+		const j=i+100;
+		let msg = `between ${j} and ${j+100}`;
+		if (j===600) msg = 'above 600';
+		if (j===0) msg = 'below 100';
+		msg = `Generated table for speed tiers ${msg} and copied it to your clipboard! Paste directly into a google doc.`;
+		msg += '<br /><br />(Ignore the text prompt, it is no longer needed)';
+		app.addPopupPrompt(msg, 'Copy Next Table', next);
+	};
+	app.addPopupPrompt('Input a comma-separated list of Pokemon for speed tiers.', 'Generate', gen);
 }catch(e){alert(e)}}();
